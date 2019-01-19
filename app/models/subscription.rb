c8b15b132163db4cc15562c2e05d33dafe0214fe
cuurjol <cuurjol@gmail.com>
2019-01-19 16:02:12 +0300

    Method email_belongs_user? from subscription.rb was edited.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -30,7 +30,7 @@ class Subscription < ApplicationRecord
 
   def email_belongs_user?
     if self.user.nil? && User.all.map(&:email).include?(user_email)
-      errors.add(:email, I18n.t('activerecord.errors.models.subscription.email_belongs_user'))
+      errors.add(:user_email, :email_belongs_user)
     end
   end
 end
