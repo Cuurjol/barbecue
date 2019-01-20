837a13f365613929c8941ab0c269de0ce8ba4ca5
cuurjol <cuurjol@gmail.com>
2019-01-20 17:44:09 +0300

    Subscriptions_controller.rb was edited.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,12 +1,15 @@
 class SubscriptionsController < ApplicationController
   before_action :set_event, only: [:create, :destroy]
   before_action :set_subscription, only: [:destroy]
-  before_action :reject_current_user, only: [:create]
 
   # POST /subscriptions
   def create
     @new_subscription = @event.subscriptions.build(subscription_params)
     @new_subscription.user = current_user
+
+    if user_signed_in? && @event.user == current_user
+      return redirect_to('events/show', alert: I18n.t('controllers.subscriptions.current_user_error'))
+    end
 
     if @new_subscription.save
       EventMailer.subscription(@event, @new_subscription).deliver_now
@@ -30,12 +33,6 @@ class SubscriptionsController < ApplicationController
   end
 
   private
-
-  def reject_current_user
-    if user_signed_in? && @event.user == current_user
-      redirect_to('events/show', alert: I18n.t('controllers.subscriptions.current_user_error'))
-    end
-  end
 
   def set_subscription
     @subscription = @event.subscriptions.find(params[:id])
